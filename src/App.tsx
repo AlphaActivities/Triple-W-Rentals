4967c255efcf61a6c7d13263a0636853c6739d51
Josh the Man <gta.alpha.activities@gmail.com>
2025-11-15 07:45:38 -0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FleetShowcase from './components/FleetShowcase';
 import LuxuryInteriors from './components/LuxuryInteriors';
-import WhyPeopleRentRVs from './components/WhyPeopleRentRVs';
 import WhyChooseUs from './components/WhyChooseUs';
 import ExperienceStrip from './components/ExperienceStrip';
 import BookingForm from './components/BookingForm';
@@ -15,7 +14,6 @@ function App() {
     <div className="bg-black">
       <Navbar />
       <HeroSection />
-      <WhyPeopleRentRVs />
       <FleetShowcase />
       <LuxuryInteriors />
       <WhyChooseUs />
